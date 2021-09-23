ab7f4825a62806b5bb070d248176f9e2dc8d9ec6
GuillaumeChk <guillaume.chekhab39@gmail.com>
2021-09-23 12:07:29 +0200

    premier commit

---
+++ backend/app.js
@@ -0,0 +1,115 @@
+const express = require('express');
+
+const app = express();
+
+// CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use('/api/posts',(req, res) => {
+   const posts = [
+    {
+      id: 1,
+      user: 'Jean Dupont',
+      text: "texte texte",
+      date: '21.09.21',
+      hour: '08:05'
+    },
+    {
+      id: 2,
+      user: 'Bertrand Jacquard',
+      text: "grege",
+      date: '21.09.21',
+      hour: '14:46'
+    },
+    {
+      id: 3,
+      user: 'Cyril Bob',
+      text: "tgegregre u ehf uezfz fuez hf izehfeuhf eizuhfuez fez ghiuez ghfiez hfi heuz hexte",
+      date: '21.09.21',
+      hour: '09:01'
+    },
+    {
+      id: 4,
+      user: 'Jean Dupont',
+      text: "texgrryeyeyreete",
+      date: '21.09.21',
+      hour: '14:46'
+    },
+  ];
+  res.status(200).json(posts);
+});
+
+app.use('/api/comments',(req, res) => {
+    const comments = [
+        {
+          id: 1,
+          postId: 1,
+          user: 'Cyril Bob',
+          text: "ça va ?",
+          date: '21.09.21',
+          hour: '14:46'
+        },
+        {
+          id: 2,
+          postId: 1,
+          user: 'Jean Dupont',
+          text: "bonjour",
+          date: '21.09.21',
+          hour: '15:46'
+        },
+        {
+          id: 3,
+          postId: 1,
+          user: 'Bertrand Jacquard',
+          text: "salut c'est moi Bertrand",
+          date: '21.09.21',
+          hour: '16:46'
+        },
+        {
+          id: 5,
+          postId: 3,
+          user: 'Cyril Bob',
+          text: "ok",
+          date: '22.09.21',
+          hour: '14:46'
+        },
+        {
+          id: 4,
+          postId: 3,
+          user: 'Bertrand Jacquard',
+          text: "oui",
+          date: '22.09.21',
+          hour: '18:46'
+        },
+        
+    ];
+   res.status(200).json(comments);
+});
+
+app.use('/api/users',(req, res) => {
+    const users = [
+        {
+          name: 'Jean Dupont',
+          mail: 'jeand@mail.fr',
+          password: 'jeanjean1'
+        },
+        {
+          name: 'Bertrand Jacquard',
+          mail: 'bertj@mail.fr',
+          password: 'bertbert2'
+        },
+        {
+          name: 'Cyril Bob',
+          mail: 'cyrib@mail.fr',
+          password: 'cyricyri3'
+        }
+    ];
+    res.status(200).json(users);
+});
+
+module.exports = app;
